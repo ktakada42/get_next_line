2c5c5ddfaacc21911443dc1cc39876e46708c4a2
ktakada42 <ktakada950711@gmail.com>
2022-06-07 21:13:57 +0900

    feat: read_ignoring_newline

--- get_next_line.c
+++ get_next_line.c
@@ -6,60 +6,30 @@
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 00:24:18 by ktakada           #+#    #+#             */
-/*   Updated: 2022/05/22 14:46:36 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/06/07 21:12:50 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-
-char	*ft_strchr(const char *s, int c)
-{
-	char	*str;
-	char	chr;
-	size_t	i;
-
-	str = (char *)s;
-	chr = (char)c;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == chr)
-			return (str + i);
-		i++;
-	}
-	if (chr == '\0')
-		return (str + i);
-	return (NULL);
-}
 
 char	*get_next_line(int fd)
 {
 	char		*buf;
-	static char	*save;
-	ssize_t		read_bytes;
+	ssize_t		cc;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (buf == NULL)
 		return (NULL);
-	read_bytes = 1;
-	printf("%ld\n", read_bytes);
-	while (ft_strchr(save, '\n') == NULL && read_bytes != 0)
+	cc = read(fd, buf, BUFFER_SIZE);
+	if (cc == -1)
 	{
-		read_bytes = read(fd, buf, BUFFER_SIZE);
-		printf("%ld\n", read_bytes);
-		if (read_bytes == -1)
-		{
-			free(buf);
-			return(NULL);
-		}
-		buf[read_bytes] = '\0';
-		save = ft_strjoin(save, buf);
+		free (buf);
+		return (NULL);
 	}
-	printf("end_while\n");
-	return (buf);
+	else
+		return (buf);
 }
 
 #include <fcntl.h>
@@ -67,11 +37,15 @@ char	*get_next_line(int fd)
 
 int	main(void)
 {
-	int	fd;
-	char  *str;
+	int		fd;
+	char	*str;
 
-	fd = open("a.txt", O_RDONLY);
-	printf("%d\n", fd);
+	fd = open("hello.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		exit(1);
+	}
 	str = get_next_line(fd);
 	printf("%s\n", str);
 	return (0);
