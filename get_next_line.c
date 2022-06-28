a16b14f0c3ee392fa7e915c86e37bf55fd4f37f2
ktakada42 <ktakada950711@gmail.com>
2022-06-28 21:03:58 +0900

    feat: need_test

--- get_next_line.c
+++ get_next_line.c
@@ -6,29 +6,45 @@
 /*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 00:24:18 by ktakada           #+#    #+#             */
-/*   Updated: 2022/06/19 21:10:13 by ktakada          ###   ########.fr       */
+/*   Updated: 2022/06/28 21:03:49 by ktakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stddef.h>
 
-ssize_t	get_lf_index_from_buf(char	*buf);
+ssize_t	get_lf_index(char	*buf);
+
+char *create_line_from_save(char *save, ssize_t lf_index);
+
+char *create_line_from_save_and_buf(char *save, char *buf, ssize_t lf_index);
 
 char	*get_next_line(int fd)
 {
 	static char	*save;
 	char		*buf;
 	char		*line;
-	char		*tmp_to_free;
 	ssize_t		cc;
 	ssize_t		lf_index;
+	bool		eof;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	line = NULL;
-	tmp_to_free = NULL;
+	eof = false;
+	lf_index = get_lf_index(save);
+	if (lf_index != -1)
+	{
+		line = create_line_from_save(save, lf_index);
+		if (line == NULL)
+			return (NULL);
+		save += lf_index + 1;
+		return (line);
+	}
 	while (true)
 	{
+		if (eof)
+			return (save);
 		buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 		if (buf == NULL)
 			return (NULL);
@@ -38,22 +54,16 @@ char	*get_next_line(int fd)
 			free (buf);
 			return (NULL);
 		}
-		/* else if (cc == 0) */
-		/* 	return (NULL); */
-		lf_index = get_lf_index_from_buf(buf);
+		if (cc < BUFFER_SIZE)
+			eof = true;
+		buf[cc] = '\0';
+		lf_index = get_lf_index(buf);
 		if (lf_index == -1)
 		{
-			tmp_to_free = save;
 			save = ft_strjoin(save, buf);
-			if (save == NULL)
-			{
-				free(buf);
-				free(line);
-				free(tmp_to_free);
-				return (NULL);
-			}
 			free(buf);
-			free(tmp_to_free);
+			if (save == NULL)
+				return (NULL);
 		}
 		else
 		{
@@ -61,22 +71,27 @@ char	*get_next_line(int fd)
 			if (line == NULL)
 			{
 				free(buf);
-				free(save);
-				free(line);
 				return (NULL);
 			}
-			line[ft_strlen(save) + lf_index] = '\0';
+			line = create_line_from_save_and_buf(save, buf, lf_index);
+			if (line == NULL)
+			{
+				free(buf);
+				return (NULL);
+			}
+			save = buf + lf_index + 1;
 			free(buf);
-			free(save);
 			return (line);
 		}
 	}
 }
 
-ssize_t	get_lf_index_from_buf(char	*buf)
+ssize_t	get_lf_index(char	*buf)
 {
 	ssize_t	i;
 
+	if (buf == NULL)
+		return (-1);
 	i = 0;
 	while (buf[i] != '\0')
 	{
@@ -87,37 +102,66 @@ ssize_t	get_lf_index_from_buf(char	*buf)
 	return (-1);
 }
 
-#include <fcntl.h>
-#include <stdio.h>
-
-int	main(int argc, char **argv)
+char *create_line_from_save(char *save, ssize_t lf_index)
 {
-	int		fd;
-	char	*str;
+	char *ret;
 
-	fd = 0;
-	if (argc == 2)
-	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
-		{
-			perror("open");
-			exit(1);
-		}
-		while (true)
-		{
-			str = get_next_line(fd);
-			if (str == NULL)
-				break ;
-			printf("%s\n", str);
-			free(str);
-		}
-		/* str = get_next_line(fd); */
-		/* printf("%s\n", str); */
-		/* free(str); */
-	}
-	close(fd);
-	printf("\n\n");
-	system("leaks -q a.out");
-	return (0);
+	ret = (char *)malloc(sizeof(char) * lf_index + 1);
+	if (ret == NULL)
+		return (NULL);
+	ft_strlcpy(ret, save, lf_index + 1);
+	return (ret);
 }
+
+char *create_line_from_save_and_buf(char *save, char *buf, ssize_t lf_index)
+{
+	char *ret;
+	char *tmp_to_free;
+
+	ret = ft_strjoin(save, buf);
+	if (ret == NULL)
+		return (NULL);
+	tmp_to_free = ret;
+	ret = create_line_from_save(ret, ft_strlen(save) + lf_index);
+	if (ret == NULL)
+	{
+		free(tmp_to_free);
+		return (NULL);
+	}
+	return (ret);
+}
+
+/* #include <fcntl.h> */
+/* #include <stdio.h> */
+
+/* int	main(int argc, char **argv) */
+/* { */
+/* 	int		fd; */
+/* 	char	*str; */
+
+/* 	fd = 0; */
+/* 	if (argc == 2) */
+/* 	{ */
+/* 		fd = open(argv[1], O_RDONLY); */
+/* 		if (fd == -1) */
+/* 		{ */
+/* 			perror("open"); */
+/* 			exit(1); */
+/* 		} */
+/* 		while (true) */
+/* 		{ */
+/* 			str = get_next_line(fd); */
+/* 			if (str == NULL) */
+/* 				break ; */
+/* 			printf("%s\n", str); */
+/* 			free(str); */
+/* 		} */
+/* 		/1* str = get_next_line(fd); *1/ */
+/* 		/1* printf("%s\n", str); *1/ */
+/* 		/1* free(str); *1/ */
+/* 	} */
+/* 	close(fd); */
+/* 	printf("\n\n"); */
+/* 	system("leaks -q a.out"); */
+/* 	return (0); */
+/* } */
